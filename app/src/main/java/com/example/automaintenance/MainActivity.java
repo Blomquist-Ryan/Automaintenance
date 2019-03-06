5bf3c202f4900f587afa19b4e2aac89a39d2cc0c
Ryan Blomquist <rblomquist96@gmail.com>
2019-03-06 12:45:41 -0700

    created all the activites with buttons to traverse  beetween them. they do not have heirarchal parents so they cant go back any

--- app/src/main/java/com/example/automaintenance/MainActivity.java
+++ app/src/main/java/com/example/automaintenance/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.automaintenance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,8 +24,15 @@ public class MainActivity extends AppCompatActivity {
             TextView text = findViewById(R.id.textView);
             text.setText(message);
     }
-    public void addToVehicleList(){
+    public void addToVehicleList(View view){
+        Intent intent = new Intent(this, AddVehicle.class);
+        startActivity(intent);
         vehicleList.add(new Vehicle());
+    }
+
+    public void toOptionsPage(View view){
+        Intent intent = new Intent(this, AddVehicle.class);
+        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/com/example/automaintenance/Options.java
@@ -0,0 +1,41 @@
+package com.example.automaintenance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+        Intent intent = getIntent();
+
+    }
+
+    public void toService(View view){
+        Intent intent = new Intent(this, AddVehicle.class);
+        startActivity(intent);
+
+    }
+
+    public void toServiceHistory(View view){
+        Intent intent = new Intent(this, AddVehicle.class);
+        startActivity(intent);
+
+    }
+
+    public void toDIY(View view){
+        Intent intent = new Intent(this, AddVehicle.class);
+        startActivity(intent);
+
+    }
+
+//    public void toParts(View view){
+//        Intent intent = new Intent(this, AddVehicle.class);
+//        startActivity(intent);
+//
+//    }
+}
