c86530620ca6f6d0f3793fbc10da3b16b41831be
Daniel Rasmussen <danrasmussen1@outlook.com>
2019-03-06 13:53:37 -0700

    Created save and load functions

--- app/src/main/java/com/example/automaintenance/MainActivity.java
+++ app/src/main/java/com/example/automaintenance/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.automaintenance;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,25 +19,49 @@ import static junit.framework.Assert.assertEquals;
 
 public class MainActivity extends AppCompatActivity {
     List<Vehicle> vehicleList = new ArrayList<>();
+    String savedVehicle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            String message = "it works";
-            TextView text = findViewById(R.id.textView);
-            text.setText(message);
+
+        load();
+
+        save();
     }
+
     public void addToVehicleList(View view){
         Intent intent = new Intent(this, AddVehicle.class);
         startActivity(intent);
-        vehicleList.add(new Vehicle());
+
+        AddVehicle vehicle = new AddVehicle();
+        Vehicle newVehicle  = vehicle.newVehicle();
+
+        vehicleList.add(newVehicle);
     }
 
     public void toOptionsPage(View view){
         Intent intent = new Intent(this, AddVehicle.class);
         startActivity(intent);
     }
+
+    public void save() {
+        Gson gson = new Gson();
+        savedVehicle = gson.toJson(vehicleList);
+        SharedPreferences mSettings = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putString(savedVehicle, "savedVehicle");
+        editor.apply();
+    }
+
+    public void load() {
+        Gson gson = new Gson();
+        SharedPreferences mSettings = this.getSharedPreferences("name_preferences.xml", 0);
+        String sharedprefs = mSettings.getString("missing","savedVehicle");
+        vehicleList = gson.fromJson(sharedprefs, new TypeToken<List<Vehicle>>(){}.getType());
+    }
+
 
 }
--- app/src/main/java/com/example/automaintenance/Vehicle.java
+++ app/src/main/java/com/example/automaintenance/Vehicle.java
@@ -1,14 +1,14 @@
 package com.example.automaintenance;
 
 public class Vehicle {
-
     // Vehicle Variables:
-    private int  vehicleMiles;
+    private int vehicleMiles;
     private String make;
     private String model;
     private int year;
     private String licensePlate;
     private String vin;
+
 
     public int getVehicleMiles() {
         return vehicleMiles;
@@ -18,17 +18,43 @@ public class Vehicle {
         this.vehicleMiles = vehicleMiles;
     }
 
-    public Vehicle() {
-        setVehicleMiles(20000);
+    public String getMake() {
+        return make;
     }
 
-    public void setNextOilChangeMiles(){
-        vehicleMiles += 3000;
+    public void setMake(String make) {
+        this.make = make;
     }
 
-    public void setNextTireRotationMiles(){
-        vehicleMiles += 5000;
+    public String getModel() {
+        return model;
     }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
 
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
 }
