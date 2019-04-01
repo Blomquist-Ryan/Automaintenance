662ef1e93d80f2d83456175dbbd00c6a04e03b22
Ryan Blomquist <rblomquist96@gmail.com>
2019-04-01 13:37:46 -0600

    services and ervices hsitory however, the saving to shared preferences causes it to crash

--- app/src/main/java/com/example/automaintenance/Service.java
+++ app/src/main/java/com/example/automaintenance/Service.java
@@ -1,16 +1,46 @@
 package com.example.automaintenance;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // The Service class is responsible for calculating
 // all of the service dates according to the vehicle's
 // mileage.
+
+/*
+        create new objects of each service type and save them to shared preferences
+        try and get shared preferences to display in service History
+ */
+
+
+
 public class Service extends AppCompatActivity {
     private Spinner Choices;
+    //String type = "";
+
+    public String getMiles() {
+        return miles;
+    }
+
+    String miles = "";
+    //String date = "";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +52,62 @@ public class Service extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Service_Options));
         Choices.setAdapter(adapter);
+
     }
-
-    private int dateOfNextService;
-
-    // Class Methods
-    public int getDateOfNextService() {
-        return dateOfNextService;
-    }
-
-    public void setDateOfNextService(int dateOfNextService) {
-        this.dateOfNextService = dateOfNextService;
-    }
-
-    public Service() {
-        dateOfNextService = 0;
-    }
-
+//
+//    private int dateOfNextService;
+//
+//    // Class Methods
+//    public int getDateOfNextService() {
+//        return dateOfNextService;
+//    }
+//
+//    public void setDateOfNextService(int dateOfNextService) {
+//        this.dateOfNextService = dateOfNextService;
+//    }
+//
+//    public Service() {
+//        dateOfNextService = 0;
+//    }
+//
     public int calculateNextService(int currentMiles) {
         return 0;
     }
+//
+   public void saveData(View view){
+
+
+        Service service = new Service();
+
+
+       EditText make = findViewById(R.id.servicemiles);
+       String newMiles = make.getText().toString();
+       service.miles = newMiles;
+       Log.i("Inside saveData", "saveData: " + service.getMiles());
+
+       Gson gson = new Gson();
+       String history = gson.toJson(service);
+
+       SharedPreferences histpref = PreferenceManager.getDefaultSharedPreferences(this);
+       SharedPreferences.Editor editor = histpref.edit();
+       editor.putString("savedHistory", history);
+       editor.apply();
+
+//       // List<Service> service = new ArrayList<>();
+//        Service service = new Service();
+////        Spinner spinner = findViewById(R.id.serviceChoices);
+////        String serviceOption = spinner.getSelectedItem().toString();
+////        service.type = serviceOption;
+//       // service = load();
+//
+//
+//
+////        ;
+////
+////
+////
+//
+    }
+
+
 }
--- app/src/main/java/com/example/automaintenance/ServiceHistory.java
+++ app/src/main/java/com/example/automaintenance/ServiceHistory.java
@@ -1,13 +1,25 @@
 package com.example.automaintenance;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServiceHistory extends AppCompatActivity {
     private Spinner Choices;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +30,33 @@ public class ServiceHistory extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Service_Options));
         Choices.setAdapter(adapter);
+
+        TextView history = findViewById(R.id.History);
+//        //List<Service> services = new ArrayList<>();
+        Service service = load();
+          //service = load();
+        history.setText(service.toString());
+
+
     }
+
+
+
+    public Service load(){Gson gson = new Gson();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String savedhistoryInfo = prefs.getString("serviceHistory", "Missing");
+
+        if (savedhistoryInfo.equals("Missing")) {
+            Log.i("Inside Load", "Couldn't load savedVehicleInfo.");
+            return null;
+        }
+        else {
+//            List<Service> history = gson.fromJson(savedhistoryInfo, new TypeToken<List<Service>>() {
+////            }.getType());
+            Service history = new Service();
+            Log.i("Inside Else:", "Added saved vehicle.");
+            return history;
+        }
+
+}
 }
