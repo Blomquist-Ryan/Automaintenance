22d1c614b6b7652a88b78b8bdaf38bfefe77fc44
Daniel Rasmussen <danrasmussen1@outlook.com>
2019-04-03 13:16:58 -0600

    Cleaned up the code in Service.java

--- app/src/main/java/com/example/automaintenance/Service.java
+++ app/src/main/java/com/example/automaintenance/Service.java
@@ -1,8 +1,6 @@
 package com.example.automaintenance;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,24 +8,11 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.util.ArrayList;
-import java.util.List;
-
-// The Service class is responsible for calculating
-// all of the service dates according to the vehicle's
-// mileage.
-
-/*
+/**
         create new objects of each service type and save them to shared preferences
         try and get shared preferences to display in service History
  */
-
-
-
 public class Service extends AppCompatActivity {
     private Spinner Choices;
     String type = "";
@@ -55,16 +40,11 @@ public class Service extends AppCompatActivity {
 
     }
 
-    public void calculateNextService() {
-    }
-//
    public void saveData(View view){
 
         EditText make = findViewById(R.id.servicemiles);
         String newMiles = make.getText().toString();
         Log.i("Inside saveData", "saveData: " + oil.getMiles());
-
-
 
         Spinner spinner = findViewById(R.id.serviceChoices);
         String serviceOption = spinner.getSelectedItem().toString();
